10d81be493b3ade586c689d7de41213f5a3475e9
Theo Sun <theo.sun@sap.com>
2020-07-14 15:46:52 +0800

    feat: #4 enhance decorator

--- src/lib/typeorm/decorators.ts
+++ src/lib/typeorm/decorators.ts
@@ -1,6 +1,8 @@
-import { ColumnOptions, Column, PrimaryColumn, EntityOptions, Entity } from 'typeorm';
+import { ColumnOptions, Column, EntityOptions, Entity } from 'typeorm';
 import { Edm } from '..';
 import { NotImplementedError } from '../error';
+import toInteger from '@newdash/newdash/toInteger';
+import { has } from '@newdash/newdash/has';
 
 
 /**
@@ -19,18 +21,34 @@ export function ODataModel(options: EntityOptions = {}) {
 /**
  * ODataColumn
  *
- * combine `Edm` decorator & typeorm decorator
+ * combine the `Edm` & `typeorm` decorator
  *
  * @param options
  */
 export function ODataColumn(options: ColumnOptions = {}) {
-  return function(object: any, propertyName: string) {
-    const { primary } = options;
+  return function(object: any, propertyName: string, propertyIndex: number) {
+    const { primary, length, precision, nullable } = options;
 
     Column(options)(object, propertyName);
 
     if (primary) {
       Edm.Key(object, propertyName);
+    }
+
+    if (length) {
+      Edm.MaxLength(toInteger(length))(object, propertyName, propertyIndex);
+    }
+
+    if (precision) {
+      Edm.Precision(toInteger(precision))(object, propertyName, propertyIndex);
+    }
+
+    if (nullable) {
+      Edm.Nullable(object, propertyName, propertyIndex);
+    }
+
+    if (has(options, 'default')) {
+      Edm.DefaultValue(options.default)(object, propertyName, propertyIndex);
     }
 
     const reflectType = Reflect.getMetadata('design:type', object, propertyName);
@@ -40,7 +58,18 @@ export function ODataColumn(options: ColumnOptions = {}) {
         Edm.String(object, propertyName);
         break;
       case Number:
-        Edm.Int32(object, propertyName);
+        switch (options.type) {
+          case 'int': case 'int2': case 'int4': case 'int8':
+            Edm.Int16(object, propertyName);
+            break;
+          case 'int64':
+            Edm.Int64(object, propertyName);
+            break;
+          default:
+            // unknown or not have type
+            Edm.Int32(object, propertyName);
+            break;
+        }
         break;
       case Boolean:
         Edm.Boolean(object, propertyName);
