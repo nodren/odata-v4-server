de3196c0dbd007b68a6e0f9c9a6d6d600483f70c
Theo Sun <theo.sun@outlook.com>
2020-08-01 10:09:55 +0800

    feat: #18

--- src/test/typeorm/integration.spec.ts
+++ src/test/typeorm/integration.spec.ts
@@ -19,17 +19,31 @@ describe('Typed OData Server Integration Test Suite', () => {
       const classRegistry = client.getEntitySet<RelStudentClassAssignment>('RelStudentClassAssignments');
 
       const t1 = await teachers.create({ name: 'turing' });
+      const t2 = await teachers.create({ name: 'issac' });
+
       const s1 = await students.create({ name: 'theo' });
+      const s2 = await students.create({ name: 'sun' });
+
       const c1 = await classes.create({
         name: 'computer science',
         teacherOneId: t1.id,
         desc: 'Computer science is the study of computation and information.'
       });
+
+      const c2 = await classes.create({
+        name: 'Theoretical physics',
+        teacherOneId: t2.id,
+        desc: 'Theoretical physics is a branch of physics that employs mathematical models and abstractions of physical objects and systems to rationalize, explain and predict natural phenomena.'
+      });
+
+
       const r1 = await classRegistry.create({ classId: c1.id, studentId: s1.id });
+      const r2 = await classRegistry.create({ classId: c2.id, studentId: s2.id });
+      const r3 = await classRegistry.create({ classId: c2.id, studentId: s1.id });
 
-      const full = await classes.retrieve(c1.id, client.newParam().expand('students($expand=student)'));
+      const c1Full = await classes.retrieve(c1.id, client.newParam().expand('students($expand=student)'));
 
-      expect(full).toMatchObject({
+      expect(c1Full).toMatchObject({
         'id': c1.id,
         'name': c1.name,
         'desc': c1.desc,
@@ -39,6 +53,37 @@ describe('Typed OData Server Integration Test Suite', () => {
             'uuid': r1.uuid,
             'studentId': s1.id,
             'classId': c1.id,
+            'student': {
+              'id': s1.id,
+              'name': 'theo',
+              'age': null
+            }
+          }
+        ]
+      });
+
+      const c2Full = await classes.retrieve(c2.id, client.newParam().expand('students($expand=student)'));
+
+      expect(c2Full).toMatchObject({
+        'id': c2.id,
+        'name': c2.name,
+        'desc': c2.desc,
+        'teacherOneId': t2.id,
+        'students': [
+          {
+            'uuid': r2.uuid,
+            'studentId': s2.id,
+            'classId': c2.id,
+            'student': {
+              'id': s2.id,
+              'name': 'sun',
+              'age': null
+            }
+          },
+          {
+            'uuid': r3.uuid,
+            'studentId': s1.id,
+            'classId': c2.id,
             'student': {
               'id': s1.id,
               'name': 'theo',
