1b8fa94eab456bcb0b4db90d4f47f214871e9b37
Theo Sun <theo.sun@sap.com>
2020-09-18 18:01:05 +0800

    fix: migration test wrong

--- test/type/migrate.spec.ts
+++ test/type/migrate.spec.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { QueryFailedError } from 'typeorm';
-import { ConnectionOptions, createTypedODataServer, IncKeyProperty, migrate, ODataModel } from '../../src';
+import { ConnectionOptions, createTypedODataServer, IncKeyProperty, migrate, ODataModel, OptionalProperty } from '../../src';
 import { createTmpMigrateConnOpt } from './utils';
 
 describe('Server Test Suite', () => {
@@ -9,7 +9,7 @@ describe('Server Test Suite', () => {
   it('should throw error when model is not sync', async () => {
 
     @ODataModel()
-    class V1 { @IncKeyProperty() id: number; }
+    class V1 { @IncKeyProperty() id: number; @OptionalProperty() value: string; }
 
     const opt: ConnectionOptions = createTmpMigrateConnOpt({
       name: 'server_creation_test_00',
@@ -22,7 +22,7 @@ describe('Server Test Suite', () => {
 
     const { services: [v1Service] } = await server.getServicesWithNewContext(V1);
 
-    await expect(() => v1Service.create({ id: 1 })).rejects.toThrow(QueryFailedError); // table not found
+    await expect(() => v1Service.create({ value: 'test' })).rejects.toThrow(QueryFailedError); // table not found
 
   });
 
@@ -30,7 +30,7 @@ describe('Server Test Suite', () => {
   it('should works fine with migration', async () => {
 
     @ODataModel()
-    class V1 { @IncKeyProperty() id: number; }
+    class V1 { @IncKeyProperty() id: number; @OptionalProperty() value: string; }
 
     const opt: ConnectionOptions = createTmpMigrateConnOpt({
       name: 'server_creation_test_01',
@@ -46,7 +46,7 @@ describe('Server Test Suite', () => {
 
     const { services: [v1Service] } = await server.getServicesWithNewContext(V1);
 
-    const obj = await v1Service.create({ id: 1 });
+    const obj = await v1Service.create({ value: 'test' });
 
     expect(obj.id).not.toBeUndefined();
 
