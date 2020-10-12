05d37b98beef9a3cffd238ef25a51e5e453ab522
Theo Sun <theo.sun@outlook.com>
2020-10-12 20:52:04 +0800

    chore(deps): update

--- test/type/school_model/Class.ts
+++ test/type/school_model/Class.ts
@@ -1,15 +1,23 @@
-import { IncKeyProperty, ODataModel, ODataNavigation, OptionalProperty, Property, withEntitySetName } from '../../../src';
+import { IncKeyProperty, ODataModel, ODataNavigation, OptionalProperty, Property, ui, withEntitySetName } from '../../../src';
 import { RelStudentClassAssignment } from './Rel';
 import { Teacher } from './Teacher';
 
 // indicate the entity set name for entity
 @withEntitySetName('Classes')
 @ODataModel()
+@ui.Label('School Classes')
 export class Class {
 
+  @ui.Label('Class ID')
+  @ui.FormField('updateForm', 'viewForm')
+  @ui.ReadOnly('updateForm')
+  @ui.TableItem(0)
   @IncKeyProperty()
   cid: number;
 
+  @ui.Label('Class Name')
+  @ui.TableItem(1)
+  @ui.TableQueryItem()
   @Property()
   name: string;
 
